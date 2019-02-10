049bbeb4b2477d10e8a0da6249987ce5b24b1fc1
PragmaTwice <e.d.boy@aliyun.com>
2019-02-10 19:16:35 +0800

    gdt: add definitions and init gdt

--- init/entry.c
+++ init/entry.c
@@ -1,15 +1,15 @@
 #include "console.h"
 #include "debug.h"
+#include "gdt.h"
 
 int kern_entry()
 {
 	init_debug();
+	init_gdt();
 
 	console_clear();
 
 	printk_color(rc_black, rc_green, "Hello, Boogie OS!\n");
-
-	panic("test");
 
 	return 0;
 }
