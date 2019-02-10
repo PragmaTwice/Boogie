ec4ed952bf82b01b4b5ff464d244c43c5145d7cf
PragmaTwice <e.d.boy@aliyun.com>
2019-02-10 21:37:49 +0800

    idt: add definitions and init idt

--- init/entry.c
+++ init/entry.c
@@ -1,15 +1,20 @@
 #include "console.h"
 #include "debug.h"
 #include "gdt.h"
+#include "idt.h"
 
 int kern_entry()
 {
 	init_debug();
 	init_gdt();
+	init_idt();
 
 	console_clear();
 
 	printk_color(rc_black, rc_green, "Hello, Boogie OS!\n");
+
+	asm volatile ("int $0x3");
+	asm volatile ("int $0x4");
 
 	return 0;
 }
